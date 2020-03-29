a80405ac48ce8855f9f27eec08a684d18086ab79
dali05 <dali09616120>
2020-03-29 01:17:19 +0100

    init Flad Project

---
+++ flad-access/src/main/java/com.lma.flad/access/FladApplication.java
@@ -0,0 +1,11 @@
+package com.lma.flad.access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FladApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FladApplication.class, args);
+    }
+}
--- flad-service/src/main/java/com/lma/flad/bvo/Hotel.java
+++ flad-service/src/main/java/com/lma/flad/bvo/Hotel.java
@@ -1,4 +1,4 @@
-package rc.domain;
+package com.lma.flad.bvo;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
