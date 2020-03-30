84238e415461b6e43a24b99056af6f3933f5f967
dali05 <dali09616120>
2020-03-30 17:03:34 +0200

    migrate spring boot to 2.0.0.RELEASE

--- flad-dao/src/main/java/com.lma.flad/repository/HotelRepository.java
+++ flad-dao/src/main/java/com.lma.flad/repository/HotelRepository.java
@@ -3,6 +3,7 @@ package com.lma.flad.repository;
 import com.lma.flad.entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
